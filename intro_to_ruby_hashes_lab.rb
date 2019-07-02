1a0571a75996ef56061f651b991dfa9179ce2c78
Andnrew Cheon <cheonandrew113@gmail.com>
2019-07-02 04:37:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ end
 def monopoly_with_second_tier
   
   monopoly_with_second_tier = {
-    :railroads => {:pieces => 4}, :names => {:names => }
+    :railroads => {:pieces => 4}, :names => {}
   }
 
 end
